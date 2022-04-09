4d8802d5c5b915f485448f0611e65541f73ef859
Carmen <carmenp.gomez99@gmail.com>
2022-04-09 10:29:22 +0200

    feat(useState): useState example

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
 import './styles.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
+import { useState } from 'react'
 
 export default function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  
+  const clickHandler = () => {
+    setTitle('updated!')
+    console.log(title)
+  }
 
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <p>{props.amount}</p>
       </div>
+      <button onClick={clickHandler}>Chage title</button>
     </Card>
   )
 }
