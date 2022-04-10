7c682c8e9aced5b7015e766db1e50f5244514d00
Carmen <carmenp.gomez99@gmail.com>
2022-04-10 19:52:42 +0200

    conditional content

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -13,6 +13,19 @@ export default function ExpensesList(props) {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === yearSelected
   })
+
+  let expensesContent = <p>No expenses found.</p>
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <div>
       <Card>
@@ -20,14 +33,7 @@ export default function ExpensesList(props) {
           selected={yearSelected}
           onSelectedDate={SelectedDateHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   )
