996d12eaab10ada22305831a9620638501b806a1
Carmen <carmenp.gomez99@gmail.com>
2022-04-10 11:01:40 +0200

    feat(year filter)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
-import ExpensesList from './components/Expenses/ExpensesList';
-import NewExpense from './components/NewExpense/NewExpense';
+import './App.css'
+import ExpensesList from './components/Expenses/ExpensesList'
+import NewExpense from './components/NewExpense/NewExpense'
 
 function App() {
   const expenses = [
-    {'title': 'Car Insurance', 'amount': 294.67, 'date': new Date(2021, 5, 12)},
-    {'title': 'Toilet paper', 'amount': 24.67, 'date': new Date(2021, 4, 10)}
-  ];
+    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 12) },
+    { title: 'Toilet paper', amount: 24.67, date: new Date(2021, 4, 10) },
+  ]
 
   const addExpenseHandler = (expense) => {
     console.log(expense)
   }
   return (
     <div className="App">
-        <NewExpense onAddExpense={addExpenseHandler} />
-        <ExpensesList items={expenses}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <ExpensesList items={expenses} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,19 +1,29 @@
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import ExpenseFilter from '../ExpenseFilter/ExpenseFilter'
+import { useState } from 'react'
 
 export default function ExpensesList(props) {
+  const [yearSelected, setYearSelected] = useState('2020')
+  //we define the function we want to pass down
+  const SelectedDateHandler = (selectedDate) => {
+    setYearSelected(selectedDate)
+  }
   return (
-    <Card>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-    </Card>
+    <div>
+      <Card>
+        <ExpenseFilter selected={yearSelected} onSelectedDate={SelectedDateHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+      </Card>
+    </div>
   )
 }
