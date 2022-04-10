b99a31f7dea65391f0e9eaa6bc121ddaaa2c8025
Carmen <carmenp.gomez99@gmail.com>
2022-04-10 09:21:44 +0200

    passing new expense to app.js

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ function App() {
     {'title': 'Car Insurance', 'amount': 294.67, 'date': new Date(2021, 5, 12)},
     {'title': 'Toilet paper', 'amount': 24.67, 'date': new Date(2021, 4, 10)}
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log(expense)
+  }
   return (
     <div className="App">
-        <NewExpense />
+        <NewExpense onAddExpense={addExpenseHandler} />
         <ExpensesList items={expenses}/>
     </div>
   );
