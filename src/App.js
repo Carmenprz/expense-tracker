94dc582d6abe40da7efb6d9c064c2eb11da83b66
Carmen <carmenp.gomez99@gmail.com>
2022-04-11 17:52:04 +0200

    new expenses data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import NewExpense from './components/NewExpense/NewExpense'
 const initial_expenses = [
   { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 12) },
   { title: 'Toilet paper', amount: 24.67, date: new Date(2021, 4, 10) },
+  { title: 'New TV', amount: 499.99, date: new Date(2021, 9, 19) },
+  { title: 'Books', amount: 55.65, date: new Date(2020, 4, 10) },
+  { title: 'Health Insurance', amount: 24.67, date: new Date(2021, 11, 10) },
+  { title: 'Toilet paper', amount: 24.67, date: new Date(2020, 4, 10) },
+  { title: 'Mom\'s present', amount: 140.67, date: new Date(2020, 9, 10) }
 ]
+
 function App() {
   const [expenses, setExpenses] = useState(initial_expenses)
 
